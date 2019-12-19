29c7e6b10c1a16376f308ecbd7659fb025b145ac
YOUZI <1483160638@qq.com>
2019-12-19 18:11:20 +0800

    1.0.0

--- RedisTest/src/main/java/com/youzi/redis/RedisSave.java
+++ RedisTest/src/main/java/com/youzi/redis/RedisSave.java
@@ -7,20 +7,27 @@ public class RedisSave {
     @Test
     public void save(){
         Jedis jedis = new Jedis("localhost");
-//        System.out.println(jedis.get("2"));
-//        jedis.set("1","youzi");
-//        Set<String> set = jedis.keys("*");
-//        Iterator<String> it = set.iterator();
-//        while(it.hasNext()){
-//            String key = it.next();
-//            System.out.println(jedis.get(key));
-//        }
-//        jedis.lpush("test","12","13","14","15");
-//        long x = jedis.scard("test");
-//        List<String> list = jedis.lrange("test",0,4);
-//        for(int i=0;i<list.size();i++){
-//            System.out.println(list.get(i));
-//        }
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+//        SFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+//        SFP  CSBSFP  CSBSFP  CSB
+
+
+
+
+
         jedis.del("1","2","3","youzi");
 
     }
